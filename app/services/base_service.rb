160dce399698494ee3a9b03e8a6525d03405c54e
Jamela Black <jamela.black@gmail.com>
2015-09-05 11:17:14 -0400

    Remove debugging code

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -2,8 +2,6 @@ module BaseService
   def send(endpoint)
     response = RestClient.get "https://#{subdomain}:#{password}@api.bigcartel.com/v1/#{endpoint}", {:accept => 'application/vnd.api+json' }
     JSON.parse(response)
-    Rails.logger.info(SUBDOMAIN: "#{subdomain}")
-    response.inspect
   end
 
   def subdomain
