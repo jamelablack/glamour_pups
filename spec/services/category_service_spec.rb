4b9e05ee757680fbc50dcff1f2dec3f28f4fb1cb
Jamela Black <jamela.black@gmail.com>
2015-09-06 01:39:56 -0400

    Prefer to mock RestCient in CategoryService unit spec over Webmock

--- spec/services/category_service_spec.rb
+++ spec/services/category_service_spec.rb
@@ -1,94 +1,46 @@
 require 'rails_helper'
 
 describe CategoryService do
+  let(:service) { described_class.new }
   let(:subdomain) { Rails.application.secrets.big_cartel_subdomain }
   let(:password) { Rails.application.secrets.big_cartel_password }
+  let(:category) { double 'category' }
+  let(:endpoint) { 'accounts/2270023/categories' }
 
-before(:each) do
-    stub_request( :get, "https://#{subdomain}:#{password}@api.bigcartel.com/v1/accounts/2270023/categories")
-      .with(
-        headers:  {
-          'Accept' => 'application/vnd.api+json',
-          'Accept-Encoding' => 'gzip, deflate',
-          'User-Agent' => 'Ruby'
-        }
-      ).to_return(status: 200, body: { "data"=> [
-    {
-      "id"=> "11593798",
-      "type"=> "categories",
-      "attributes"=> {
-        "name"=> "Happy",
-        "permalink"=> "happy",
-        "position"=> 1
-      },
-      "links"=> {
-        "self"=> "https://api.bigcartel.com/v1/accounts/2270023/categories/11593798"
-      }
-    },
-    {
-      "id"=> "11593801",
-      "type"=> "categories",
-      "attributes"=> {
-        "name"=> "Sad",
-        "permalink"=> "sad",
-        "position"=> 2
-      },
-      "links"=> {
-        "self"=> "https://api.bigcartel.com/v1/accounts/2270023/categories/11593801"
-      }
-    },
-    {
-      "id"=> "11593804",
-      "type"=> "categories",
-      "attributes"=> {
-        "name"=> "Funny",
-        "permalink"=> "funny",
-        "position"=> 3
-      },
-      "links"=> {
-        "self"=> "https://api.bigcartel.com/v1/accounts/2270023/categories/11593804"
-      }
-    },
-    {
-      "id"=> "11593807",
-      "type"=> "categories",
-      "attributes"=> {
-        "name"=> "Warm & Fuzzies",
-        "permalink"=> "warm-fuzzies",
-        "position"=> 4
-      },
-      "links"=> {
-        "self"=> "https://api.bigcartel.com/v1/accounts/2270023/categories/11593807"
-      }
-    },
-    {
-      "id"=> "11593867",
-      "type"=> "categories",
-      "attributes"=> {
-        "name"=> "Thought Provoking",
-        "permalink"=> "thought-provoking",
-        "position"=> 5
-      },
-      "links"=> {
-        "self"=> "https://api.bigcartel.com/v1/accounts/2270023/categories/11593867"
-      }
-    }
-  ],
-  "meta"=> {
-    "count"=> "5"
-  },
-  "included"=> [],
-  "links"=> {}}.to_json, headers: {})
+  let(:url) do
+    "https://#{subdomain}:#{password}@api.bigcartel.com/v1/#{endpoint}"
+  end
+
+  let(:headers) do
+    { :accept => 'application/vnd.api+json' }
+  end
+
+  before(:each) do
+    expect(RestClient).to receive(:get).with(url, headers) do
+      {
+        data: [
+          {
+            id: '123456',
+            type: 'blah',
+           attributes: {
+              foo: 'bar',
+              fizz: 'buzz'
+            }
+          }
+        ]
+      }.to_json
+    end
   end
 
 
 
-  describe ".all" do
-    it "should return a store's categories" do
-      categories = described_class.new.all
 
-      expect(categories.first.id).to eq '11593798'
-      expect(categories.first.name).to eq "Happy"
+  describe "#all" do
+    it "should return a store's product" do
+      expect(Category).to receive(:new)
+        .with({ id: '123456', type: 'blah', foo: 'bar', fizz: 'buzz' }) { category }
+
+      expect(service.all.first).to eql category
     end
   end
 end
